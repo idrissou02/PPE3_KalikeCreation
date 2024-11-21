7a7394279c571abdd5bcc88af7443be2adb763e4
zakaria78180 <zakaria.azr789@gmail.com>
2024-11-21 10:53:59 +0100

    ficheObjetDeco

--- src/Controller/ObjetDecoController.php
+++ src/Controller/ObjetDecoController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Bougie;
-use App\Repository\BougieRepository;
 use App\Repository\ObjetDecorationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +9,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ObjetDecoController extends AbstractController
 {
+  
     #[Route('/objetdecoration', name: 'app_objetdecoration', methods: 'GET')]
     public function listeObjetDeco(ObjetDecorationRepository $repo): Response
     {
-        $objetDeco=$repo->findAll();
+        $objetDeco = $repo->findAll();
         return $this->render('objetdecoration/listeObjetdecoration.html.twig', [
             'LesObjetsDeco' => $objetDeco
+        ]);
+    }
+
+    #[Route('/objetdecoration/{id}', name: 'app_fiche_objetdecoration', methods: 'GET')]
+    public function ficheObjetDeco(int $id, ObjetDecorationRepository $repo): Response
+    {
+     
+        $objetDeco = $repo->find($id);
+        if (!$objetDeco) {
+            throw $this->createNotFoundException('Objet décoratif non trouvé');
+        }
+        return $this->render('objetdecoration/ficheObjetdecoration.html.twig', [
+            'objet' => $objetDeco
         ]);
     }
 }
