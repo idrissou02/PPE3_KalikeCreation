d2dcd0d5a249c9d7bff2202d2bdae83fc9e7aed5
Idriss SOUHIL MOHAMED <idriss.souhil@btssio.dedyn.io>
2024-11-21 12:14:17 +0100

    modif du controllerPoudre

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Bougie;
+use App\Entity\Poudre;
 use App\Entity\ObjetDecoration;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -14,6 +15,8 @@ class AppFixtures extends Fixture
     {
         
         $this->loadBougies($manager);
+
+        $this->loadPoudre($manager);
 
         $this->loadObjetsDecoration($manager);
     }
@@ -61,16 +64,16 @@ class AppFixtures extends Fixture
 
         foreach ($lesPoudres as $value)
         {
-            $poudre=new Bougie();
+            $poudre=new Poudre();
             $poudre    
                         ->setNom($value[1])
                         ->setMateriaux($value[2])
                         ->setPrix($value[3])
                         ->setCouleur($value[4])     
-                        ->setPoid($value[5])
-                        ->setDdv($value[6])
+                        ->setPoids(3.4)
+                        ->setDuréeDeVie($value[6])
                         ->setImage('https://lorempicture.point-sys.com/400/300/'.mt_rand(1,30));
-                        $manager->persist($bougie);
+                        $manager->persist($poudre);
         }
             $manager->flush();
     }
