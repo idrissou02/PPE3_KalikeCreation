da5dac050de60b6f9ac13ce0c4370dcd46265fe1
zakaria78180 <zakaria.azr789@gmail.com>
2024-11-28 11:11:16 +0100

    DiffuseurVoiture fin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Faker\Factory;
 use App\Entity\Bougie;
 use App\Entity\ObjetDecoration;
+use App\Entity\DiffuseurVoiture;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -81,14 +82,16 @@ class AppFixtures extends Fixture
 
         foreach ($LesDiffuseursVoiture as $value)
         {
-            $DiffuseurVoiture=new Bougie();
-            $diffuseur    
+            $DiffuseurVoiture=new DiffuseurVoiture();
+            $DiffuseurVoiture    
                         ->setNom($value[1])
                         ->setMateriaux($value[2])
                         ->setPrix($value[3])
                         ->setCouleur($value[4])     
-                        ->setPoid($value[5])
-                        ->setDdv($value[6])
+                        ->setPoids($value[5])
+                        ->setDuréeDeVie($value[6])
+                        ->setTaille($value[7])
+                        ->setDescription($value[8])
                         ->setImage('https://lorempicture.point-sys.com/400/300/'.mt_rand(1,30));
                         $manager->persist($diffuseur);
         }
