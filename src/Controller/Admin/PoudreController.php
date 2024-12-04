50632c95468812c38ba662784897a425d655ae4e
idrissou02 <idriss.souhil@gmail.com>
2024-12-04 11:13:43 +0100

    pour changer le base.html du main

--- src/Controller/Admin/PoudreController.php
+++ src/Controller/Admin/PoudreController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PoudreRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PoudreController extends AbstractController
 {
-    #[Route('Admin/poudre', name: 'Admin_poudres', methods: 'GET')]
-    public function listePoudre(PoudreRepository $repo): Response
+    #[Route('/admin/poudre', name: 'admin_poudre', methods:"GET")]
+    public function listePoudres(PoudreRepository $repo, PaginatorInterface $paginator, Request $request): Response
     {
-        $poudres=$paginator->paginate(
-            $repos->listePoudresCompletePaginee(),
-            $request->query->getInt('page',1),/*page number*/
-            9/*limite per page*/
+        $poudres = $paginator->paginate(
+            $repo->listePoudreCompletePaginee(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            9 /*limit per page*/
         );
-        return $this->render('poudre/listePoudres.html.twig', [
-            'LesPoudres' => $poudres
+        return $this->render('admin/poudre/listePoudre.html.twig', [
+            'lesPoudres' => $poudres
         ]);
     }
 }
