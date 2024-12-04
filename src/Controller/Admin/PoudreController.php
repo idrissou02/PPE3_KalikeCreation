70dcf5eba37b4f6ffd57e5381df86e09360269f9
idrissou02 <idriss.souhil@gmail.com>
2024-12-04 14:15:11 +0100

    pour cedrik de merde

--- src/Controller/Admin/PoudreController.php
+++ src/Controller/Admin/PoudreController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Repository\PoudreRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,13 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PoudreController extends AbstractController
 {
     #[Route('/admin/poudre', name: 'admin_poudre', methods:"GET")]
-    public function listePoudres(PoudreRepository $repo, PaginatorInterface $paginator, Request $request): Response
+    public function listePoudres(PoudreRepository $repo): Response
     {
-        $poudres = $paginator->paginate(
-            $repo->listePoudreCompletePaginee(), /* query NOT result */
-            $request->query->getInt('page', 1), /*page number*/
-            9 /*limit per page*/
-        );
+        $poudres=$repo->listePoudres();
         return $this->render('admin/poudre/listePoudre.html.twig', [
             'lesPoudres' => $poudres
         ]);
