7facbd7c7e26b242abf08221ae797c771645b03f
idrissou02 <idriss.souhil@gmail.com>
2024-12-18 15:08:01 +0100

    juste pour le connard de cedrik

--- src/Form/PoudreType.php
+++ src/Form/PoudreType.php
@@ -7,57 +7,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PoudreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('materiaux',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('prix',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('couleur',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('poids',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('dureeDeVie',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('taille',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('image',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ])
-            ->add('description',TextType::class,[
-                'attr' => ['class' => 'text form-control']  
-                    
-                
-            ]);
+            ->add('image')
+            ->add('nom')
+            ->add('prix')
+            ->add('materiaux')
+            ->add('couleur')
+            ->add('dureeDeVie')
+            ->add('taille')
+            ->add('description');
+            // ->add('valdier', SubmitType::class);
+ 
             
         
     }
