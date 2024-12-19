3bbad432baa1ed88f28ac967e16d24c1f689403c
CedrikSI <cedrik.simonin@gmail.com>
2024-12-19 11:01:57 +0100

    j'espère ça fonctionne

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\VarDumper\Caster\SymfonyCaster;
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
@@ -12,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
