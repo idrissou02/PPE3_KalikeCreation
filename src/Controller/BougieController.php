a946b0a8c7ca90a89218e80f99515fe92667294a
Escape <simonin;cedrik@gmail.com>
2025-01-16 09:21:26 +0100

    barre de recherche toute simple seulement sur le nom d'une bougie

--- src/Controller/BougieController.php
+++ src/Controller/BougieController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Bougie;
+use App\Form\FiltreBougieType;
 use App\Repository\BougieRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,17 +15,19 @@ class BougieController extends AbstractController
     #[Route('/bougie', name: 'bougie', methods: 'GET')]
     public function listeBougie(BougieRepository $repo): Response
     {
+         
         $bougies=$repo->ListeBougies();
         return $this->render('bougie/listeBougies.html.twig', [
-            'LesBougies' => $bougies
+            'LesBougies' => $bougies,
         ]);
     }
     
     #[Route('/bougie/{id}', name: 'ficheBougie', methods: 'GET')]
-    public function FicheBougie(Bougie $bougie)     
+    public function FicheBougie(Bougie $bougie): Response    
     {
         return $this->render('bougie/FicheBougie.html.twig', [
             'LaBougie' => $bougie
+            
         ]);
     }
 
