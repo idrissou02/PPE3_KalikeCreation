84f4d0b014ff2cc723bafe778caceace7731fe43
Escape <simonin;cedrik@gmail.com>
2025-02-05 11:37:39 +0100

    le panier fonctionne tu peux suprr et payer maintenant :D bien sur le truc de prelevement fonctionne pas pour de vrai

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -11,11 +11,14 @@ class Cart
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: "integer")]
     private ?int $id = null;
 
     #[ORM\OneToMany(targetEntity: CartItem::class, mappedBy: 'cart', cascade: ['persist', 'remove'])]
     private Collection $items;
+
+    #[ORM\Column(type: "boolean")]
+    private bool $paid = false;
 
     public function __construct()
     {
@@ -32,13 +35,36 @@ class Cart
         return $this->items;
     }
 
-    public function addItem(Produit $produit): self
+    public function addItem(CartItem $item): self
     {
-        $item = new CartItem();
-        $item->setBougie($produit);
-        $item->setCart($this);
-        $this->items->add($item);
+        if (!$this->items->contains($item)) {
+            $this->items[] = $item;
+            $item->setCart($this);
+        }
 
+        return $this;
+    }
+
+    public function removeItem(CartItem $item): self
+    {
+        if ($this->items->removeElement($item)) {
+            // set the owning side to null (unless already changed)
+            if ($item->getCart() === $this) {
+                $item->setCart(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function isPaid(): bool
+    {
+        return $this->paid;
+    }
+
+    public function setPaid(bool $paid): self
+    {
+        $this->paid = $paid;
         return $this;
     }
 }
