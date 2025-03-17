4f8b537d1fe2e7e1630a5184e19681659ef98843
idrissou02 <idriss.souhil@gmail.com>
2025-03-17 15:49:22 +0100

    pour monsieur Rolland

---
+++ tests/appTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class appTest extends TestCase
+{
+    public function testTestsAreWorking()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
+
+
